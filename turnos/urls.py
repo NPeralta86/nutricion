3c5a47e9b59e8a68c0eb1ff20c9d6a966f066a49
NPeralta86 <nicolas-peralta@outlook.com>
2023-05-21 18:17:36 -0300

    Configuración de incorporación de archivos media

--- turnos/urls.py
+++ turnos/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -24,3 +26,5 @@ urlpatterns = [
     path('', include('agenda.urls')),
     path('', include('pacientes.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
