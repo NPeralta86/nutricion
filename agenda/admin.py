762c5640d875f29b9d883076161744a54f20775e
NPeralta86 <nicolas-peralta@outlook.com>
2023-05-26 13:16:36 -0300

    correcciones generales

--- agenda/admin.py
+++ agenda/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import pacientes, agenda, ficha
 
 # Register your models here.
-admin.site.register(pacientes)
+# admin.site.register(pacientes)
 admin.site.register(agenda)
 admin.site.register(ficha)
